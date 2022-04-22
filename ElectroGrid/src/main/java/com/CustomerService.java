6351cc53dd517566ce81ea18e195e078b3bf0d88
SubhashanaSS <subhashana201@gmail.com>
2022-04-23 00:27:43 +0530

    Get method

--- ElectroGrid/src/main/java/com/CustomerService.java
+++ ElectroGrid/src/main/java/com/CustomerService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import model.Customer;
 
@@ -10,4 +13,10 @@ public class CustomerService {
 	
 	Customer customerObj = new Customer();
 
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readCustomers() {
+		return customerObj.readCustomers();
+	}
 }
