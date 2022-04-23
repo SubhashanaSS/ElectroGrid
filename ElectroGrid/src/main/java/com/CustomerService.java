5ff80ba454e44720d980fe139582ce132bb394cc
SubhashanaSS <subhashana201@gmail.com>
2022-04-23 23:33:13 +0530

    Customer Post Method

--- ElectroGrid/src/main/java/com/CustomerService.java
+++ ElectroGrid/src/main/java/com/CustomerService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,5 +21,16 @@ public class CustomerService {
 	@Produces(MediaType.TEXT_HTML)
 	public String readCustomers() {
 		return customerObj.readCustomers();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertCustomer(@FormParam("CustomerName") String CustomerName,
+			@FormParam("CustomerAddress") String CustomerAddress, @FormParam("CustomerEmail") String CustomerEmail,
+			@FormParam("CustomerContact") String CustomerContact) {
+		String output = customerObj.insertCustomer(CustomerName, CustomerEmail, CustomerAddress, CustomerContact);
+		return output;
 	}
 }
