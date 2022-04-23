9fdd8bbb260570fee9556c4528b1294d18ee67d8
SubhashanaSS <subhashana201@gmail.com>
2022-04-24 00:45:09 +0530

    Customer Put method

--- ElectroGrid/src/main/java/com/CustomerService.java
+++ ElectroGrid/src/main/java/com/CustomerService.java
@@ -4,9 +4,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.Customer;
 
@@ -31,6 +35,24 @@ public class CustomerService {
 			@FormParam("CustomerAddress") String CustomerAddress, @FormParam("CustomerEmail") String CustomerEmail,
 			@FormParam("CustomerContact") String CustomerContact) {
 		String output = customerObj.insertCustomer(CustomerName, CustomerAddress, CustomerEmail, CustomerContact);
+		return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateCustomer(String customerData) {
+		// Convert the input string to a JSON object
+		JsonObject customerObject = new JsonParser().parse(customerData).getAsJsonObject();
+		// Read the values from the JSON object
+		String CustomerID = customerObject.get("CustomerID").getAsString();
+		String CustomerName = customerObject.get("CustomerName").getAsString();
+		String CustomerAddress = customerObject.get("CustomerAddress").getAsString();
+		String CustomerEmail = customerObject.get("CustomerEmail").getAsString();
+		String CustomerContact = customerObject.get("CustomerContact").getAsString();
+		String output = customerObj.updateCustomer(CustomerID, CustomerName, CustomerAddress, CustomerEmail,
+				CustomerContact);
 		return output;
 	}
 }
