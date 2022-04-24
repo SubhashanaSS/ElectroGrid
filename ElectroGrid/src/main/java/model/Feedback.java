2249c3ad8739645a972274b0997f6240b15f744b
Ishiwarani <ishiwaranipriyabhashi@gmail.com>
2022-04-25 00:37:34 +0530

    DB Connect

---
+++ ElectroGrid/src/main/java/model/Feedback.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Feedback {
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electrogrid", "root", "");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+	
+	
+}
