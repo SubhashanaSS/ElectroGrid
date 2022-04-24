e4950f344f4d05f902e265d2e8565d09a4bb4a9a
Ishiwarani <ishiwaranipriyabhashi@gmail.com>
2022-04-25 00:54:36 +0530

    Feedback Post method

--- ElectroGrid/src/main/java/com/FeedbackService.java
+++ ElectroGrid/src/main/java/com/FeedbackService.java
@@ -1,5 +1,28 @@
 package com;
 
-public class FeedbackService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import model.Feedback;
+
+@Path("/Feedbacks")
+
+public class FeedbackService {
+		
+	Feedback feedbackObj = new Feedback();
+	
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertCustomer(@FormParam("CustomerName") String CustomerName,
+				@FormParam("CustomerEmail") String CustomerEmail, @FormParam("Rate") String Rate,
+				@FormParam("FeedbackNotes") String FeedbackNotes) {
+			String output = feedbackObj.insertFeedback(CustomerName, CustomerEmail, Rate, FeedbackNotes);
+			return output;
+		}
 }
--- ElectroGrid/src/main/java/model/Feedback.java
+++ ElectroGrid/src/main/java/model/Feedback.java
@@ -2,6 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Feedback {
 	public Connection connect() {
@@ -20,5 +22,35 @@ public class Feedback {
 		return con;
 	}
 	
-	
+	// insert feedback
+			public String insertFeedback(String name, String email, String rate, String notes) {
+				Connection con = connect();
+				String output = "";
+				if (con == null) {
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = " insert into feedbacks (`FeedbackID`,`CustomerName`,`CustomerEmail`,`Rate`,`FeedbackNotes`)"
+						+ " values (?, ?, ?, ?, ?)";
+				PreparedStatement preparedStmt;
+				try {
+					preparedStmt = con.prepareStatement(query);
+
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, name);
+					preparedStmt.setString(3, email);
+					preparedStmt.setString(4, rate);
+					preparedStmt.setString(5, notes);
+
+					preparedStmt.execute();
+					con.close();
+					output = "Inserted successfully";
+				} catch (SQLException e) {
+					output = "Error while inserting";
+					System.err.println(e.getMessage());
+				}
+
+				return output;
+			}
 }
