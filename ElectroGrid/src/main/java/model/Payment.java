b6f0efcc9cd1c88ccbb14a6c21b33ac148c243e7
prabhavi <prabhavipanchali0709@gmail.com>
2022-04-25 22:57:34 +0530

    Payment post method

--- ElectroGrid/src/main/java/model/Payment.java
+++ ElectroGrid/src/main/java/model/Payment.java
@@ -23,5 +23,39 @@ public class Payment {
 
 		return con;
 	}
+	
+	// insert payment
+			public String insertPayment(String amount, String number, String type, String date) {
+				Connection con = connect();
+				String output = "";
+				if (con == null) {
+					return "Error while connecting to the database";
+
+				}
+
+				// create a prepared statement
+				String query = " insert into payments (`PaymentID`,`Amount`,`PaymentCardNo`,`PaymentType`,`PaymentDate`)"
+						+ " values (?, ?, ?, ?, ?)";
+
+				PreparedStatement preparedStmt;
+				try {
+					preparedStmt = con.prepareStatement(query);
+
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, amount);
+					preparedStmt.setString(3, number);
+					preparedStmt.setString(4, type);
+					preparedStmt.setString(5, date);
+
+					preparedStmt.execute();
+					con.close();
+					output = "Inserted successfully";
+				} catch (SQLException e) {
+					output = "Error while inserting";
+					System.err.println(e.getMessage());
+				}
+
+				return output;
+			}
 
 }
