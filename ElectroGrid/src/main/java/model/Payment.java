7b1f4735b664dd7ff5006f29057213591cd0ebd1
pinky <pinky@DESKTOP-S8N3244>
2022-04-25 22:46:37 +0530

    Payment DB connect

---
+++ ElectroGrid/src/main/java/model/Payment.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Payment {
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electrogrid", "root", "");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+}
