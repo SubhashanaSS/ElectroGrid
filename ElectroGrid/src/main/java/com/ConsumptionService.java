b436f62f4c9921e31b7cf9d9b070ab60f42e8677
1a11a <86703816+1a11a@users.noreply.github.com>
2022-04-26 19:17:14 +0530

    Consumption_Management

---
+++ ElectroGrid/src/main/java/com/ConsumptionService.java
@@ -0,0 +1,76 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Consumption;
+
+@Path("/Consumptions")
+
+public class ConsumptionService {
+	
+	Consumption consumptionObj = new Consumption();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertConsumption(@FormParam("CustomerName") String CustomerName,
+			@FormParam("CustomerUsage") String CustomerUsage, @FormParam("Price") String Price,
+			@FormParam("CustomerType") String CustomerType) {
+		String output = consumptionObj.insertConsumption(CustomerName, CustomerUsage, Price, CustomerType);
+		return output;
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readConsumption() {
+		return consumptionObj.readConsumption();
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateConsumption(String consumptionData) {
+		// Convert the input string to a JSON object
+		JsonObject consumptionObject = new JsonParser().parse(consumptionData).getAsJsonObject();
+		// Read the values from the JSON object
+		String ConsumptionID = consumptionObject.get("ConsumptionID").getAsString();
+		String CustomerName = consumptionObject.get("CustomerName").getAsString();
+		String CustomerUsage = consumptionObject.get("CustomerUsage").getAsString();
+		String Price = consumptionObject.get("Price").getAsString();
+		String CustomerType = consumptionObject.get("CustomerType").getAsString();
+		String output = consumptionObj.updateConsumption(ConsumptionID, CustomerName, CustomerUsage, Price , CustomerType);
+		return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteConsumption(String consumptionData) {
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(consumptionData, "", Parser.xmlParser());
+
+		// Read the value from the element <itemID>
+		String ConsumptionID = doc.select("ConsumptionID").text();
+		String output = consumptionObj.deleteConsumption(ConsumptionID);
+		return output;
+	}
+}
---
+++ ElectroGrid/src/main/java/model/Consumption.java
@@ -0,0 +1,161 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Consumption {
+	
+		public Connection connect() {
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electrogrid", "root", "");
+				// For testing
+				System.out.print("Successfully connected");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}
+		
+		//insert method
+		public String insertConsumption(String name, String usage, String price, String type) {
+			Connection con = connect();
+			String output = "";
+			if (con == null) {
+				return "Error while connecting to the database";
+
+			}
+
+			// create a prepared statement
+			String query = " insert into consumptions (`ConsumptionID`,`CustomerName`,`CustomerUsage`,`Price`,`CustomerType`)"
+					+ " values (?, ?, ?, ?, ?)";
+
+			PreparedStatement preparedStmt;
+			try {
+				preparedStmt = con.prepareStatement(query);
+
+				preparedStmt.setInt(1, 0);
+				preparedStmt.setString(2, name);
+				preparedStmt.setString(3, usage);
+				preparedStmt.setString(4, price);
+				preparedStmt.setString(5, type);
+
+				preparedStmt.execute();
+				con.close();
+				output = "Inserted successfully";
+			} catch (SQLException e) {
+				output = "Error while inserting";
+				System.err.println(e.getMessage());
+			}
+
+			return output;
+		}
+		
+		//read method
+		public String readConsumption() {
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+				}
+				// Prepare the html table to be displayed
+				output = "<table border='1'><tr><th>Customer Name</th><th>Customer Usage</th>" + "<th>Price</th>"
+						+ "<th>Customer Type</th></tr>";
+
+				String query = "select * from consumptions";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next()) {
+					String ConsumptionID = Integer.toString(rs.getInt("ConsumptionID"));
+					String CustomerName = rs.getString("CustomerName");
+					String CustomerUsage = rs.getString("CustomerUsage");
+					String Price = rs.getString("Price");
+					String CustomerType = rs.getString("CustomerType");
+
+					// Add into the html table
+					output += "<tr><td>" + CustomerName + "</td>";
+					output += "<td>" + CustomerUsage + "</td>";
+					output += "<td>" + Price + "</td>";
+					output += "<td>" + CustomerType + "</td>";
+
+				}
+				con.close();
+
+				// Complete the html table
+				output += "</table>";
+			} catch (Exception e) {
+				output = "Error while reading the consumption Details.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+		
+		//update method
+		public String updateConsumption(String ID, String name, String usage, String price, String type)
+
+		{
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for updating.";
+				}
+
+				String query = " update consumptions set CustomerName= ? , CustomerUsage = ? , Price = ? , CustomerType = ?  where ConsumptionID = ? ";
+
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setString(1, name);
+				preparedStmt.setString(2, usage);
+				preparedStmt.setString(3, price);
+				preparedStmt.setString(4, type);
+				preparedStmt.setInt(5, Integer.parseInt(ID));
+
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				output = "Updated successfully";
+			} catch (Exception e) {
+				output = "Error while updating the consumption Details.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+		
+		// delete method
+				public String deleteConsumption(String ConsumptionID) {
+					String output = "";
+					try {
+						Connection con = connect();
+						if (con == null) {
+							return "Error while connecting to the database for deleting.";
+						}
+						// create a prepared statement
+						String query = "delete from consumptions where ConsumptionID=?";
+						PreparedStatement preparedStmt = con.prepareStatement(query);
+
+						// binding values
+						preparedStmt.setInt(1, Integer.parseInt(ConsumptionID));
+
+						// execute the statement
+						preparedStmt.execute();
+						con.close();
+						output = "Deleted successfully";
+					} catch (Exception e) {
+						output = "Error while deleting the consumption Details.";
+						System.err.println(e.getMessage());
+					}
+					return output;
+				}
+}
